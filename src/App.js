92515454846aaae31c6969fc32dc04f96197d432
ChxDesk <chackop@gmail.com>
2021-02-18 17:40:28 +0000

    Postman app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Button, MenuItem, Select, TextField } from "@material-ui/core";
 
 function App() {
+  const [inputUrl, setInputUrl] = React.useState("");
+  const [httpType, setHttpType] = React.useState("GET");
+  const [response, setResponse] = React.useState({});
+  const [postBody, setPostBody] = React.useState({});
+
+  const marginStyles = {
+    margin: "5px",
+  };
+
+  const btnHandler = async () => {
+    const fetchParams = {
+      method: httpType,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: httpType === "POST" ? JSON.stringify(postBody) : null,
+    };
+
+    const urlResp = await fetch(inputUrl, fetchParams).then((resp) =>
+      resp.json()
+    );
+
+    console.log("urlResp", urlResp);
+    setResponse(urlResp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">
+        <TextField
+          value={inputUrl}
+          style={marginStyles}
+          onChange={(evt) => setInputUrl(evt.target.value)}
+          label="Input URL"
+          variant="outlined"
+        />
+
+        <Select
+          value={httpType}
+          style={marginStyles}
+          label="Http Method"
+          onChange={(evt) => setHttpType(evt.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value={"GET"}>Get</MenuItem>
+          <MenuItem value={"POST"}>Post</MenuItem>
+        </Select>
+
+        {httpType === "POST" && (
+          <TextField
+            value={postBody}
+            style={marginStyles}
+            onChange={(evt) => setPostBody(evt.target.value)}
+            label="Body"
+            variant="outlined"
+          />
+        )}
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={btnHandler}
+          style={marginStyles}
+        >
+          Submit
+        </Button>
+
+        <pre style={marginStyles}>{JSON.stringify(response, null, 2)}</pre>
+      </div>
     </div>
   );
 }
